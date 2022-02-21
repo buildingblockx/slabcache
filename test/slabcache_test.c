1d2ac9766af4060fca285c1697823fcc7c9af2b6
vernon2gh <vernon2gm@gmail.com>
2022-02-21 13:33:38 +0800

    Add test demo

---
+++ test/slabcache_test.c
@@ -0,0 +1,79 @@
+#include <memory/allocator/slabcache.h>
+#include <string.h>
+
+void test_alloc_8bytes(void)
+{
+	struct slab_cache *s;
+	unsigned int object_size;
+	unsigned int align;
+	unsigned long *buf;
+
+	printf("Enter %s\n", __func__);
+
+	object_size = sizeof(unsigned long);
+	align = 8;
+	printf("\tslab cache create, object size %d, align %d\n",
+		object_size, align);
+	s = slab_cache_create("unsigned long", object_size, align, 0);
+
+	printf("\tslab cache alloc\n");
+	buf = slab_cache_alloc(s, 0);
+	*buf = 0x123456;
+	printf("\tbuf address %p, buf values 0x%lx\n", buf, *buf);
+
+	printf("\tslab cache free\n");
+	slab_cache_free(s, buf);
+
+	printf("\tslab cache destroy\n");
+	slab_cache_destroy(s);
+
+	printf("Exit %s\n", __func__);
+}
+
+struct structA {
+	char name[10];
+	int id;
+};
+
+void test_alloc_structA(void)
+{
+	struct slab_cache *s;
+	unsigned int object_size;
+	unsigned int align;
+	struct structA *buf;
+
+	printf("Enter %s\n", __func__);
+
+	object_size = sizeof(struct structA);
+	align = 8;
+	printf("\tslab cache create, object size %d, align %d\n",
+		object_size, align);
+	s = slab_cache_create("struct structA", object_size, align, 0);
+
+	printf("\tslab cache alloc\n");
+	buf = slab_cache_alloc(s, 0);
+	strcpy(buf->name, "test");
+	buf->id = 0xaa;
+	printf("\tbuf address %p, buf->name %s, buf->id 0x%x\n",
+		buf, buf->name, buf->id);
+
+	printf("\tslab cache free\n");
+	slab_cache_free(s, buf);
+
+	printf("\tslab cache destroy\n");
+	slab_cache_destroy(s);
+
+	printf("Exit %s\n", __func__);
+}
+
+int slab_cache_allocator_test(void)
+{
+	printf("\nslab cache allocator init\n");
+	slab_cache_allocator_init();
+
+	test_alloc_8bytes();
+	test_alloc_structA();
+
+	return 0;
+}
+
